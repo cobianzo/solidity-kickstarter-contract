e0565f5d08e9f65a546a62efd4fa328c2249f464
Cobianzo <cobianzo@gmail.com>
2022-05-01 18:59:50 +0200

    Several pages created, connection to the Contracts Factory and Campign, working in contribution button now

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -1,10 +1,8 @@
 pragma solidity ^0.4.17;
 
-
 // contract that creates of Campaigns contracts.
 // For security, we can't allow the user to create a Campaign contract, because he could modify the bytecode and inject malicious actions, like paying himself.
 contract CampaignFactory {
-
     // this contract also gives us access to all Campaigns, saved in an array.
 
     address[] public deployedCampaigns;
@@ -12,34 +10,36 @@ contract CampaignFactory {
     // no constructor
 
     // creates a campaign contract, and saves it in the campaigns array.
-    function createCampaign(uint minimum) public {
+    function createCampaign(uint256 minimum) public {
         address theManager = msg.sender;
         address newCampaign = new Campaign(minimum, theManager); // the sender is the contract, but we need to assign the manager to the creator of the contract
         deployedCampaigns.push(newCampaign);
     }
 
-    function getDeployedCampaigns() public view returns(address[]) {
+    function getDeployedCampaigns() public view returns (address[]) {
         return deployedCampaigns;
     }
-
 }
 
-
+/**
+    Everytime I modify this code, run node compile.js to compile it, and node deploy.js to deploy and grab the address.
+    You can test it first in remix before doing all that.
+ */
 contract Campaign {
     struct Request {
         string description;
-        uint value;
+        uint256 value;
         address recipient;
         bool complete;
-        uint approvalCount;
-        mapping(address => bool) approvals;
+        uint256 approvalCount;
+        mapping(address => bool) approvals; // people who have approved the request
     }
 
     Request[] public requests;
     address public manager;
-    uint public minimumContribution;
+    uint256 public minimumContribution;
     mapping(address => bool) public approvers;
-    uint public approversCount;
+    uint256 public approversCount;
 
     // modifier (helper) to add in some functions where we want only the manager of the contract to call.
     modifier restricted() {
@@ -48,7 +48,7 @@ contract Campaign {
     }
 
     // constructor
-    function Campaign(uint minimum, address creator) public {
+    function Campaign(uint256 minimum, address creator) public {
         manager = creator; // we can't use msg.sender, because it's the address of the contract, not the creator.
         minimumContribution = minimum;
     }
@@ -60,19 +60,23 @@ contract Campaign {
         approversCount++;
     }
 
-    function createRequest(string description, uint value, address recipient) public restricted {
+    function createRequest(
+        string description,
+        uint256 value,
+        address recipient
+    ) public restricted {
         Request memory newRequest = Request({
-           description: description,
-           value: value,
-           recipient: recipient,
-           complete: false,
-           approvalCount: 0
+            description: description,
+            value: value,
+            recipient: recipient,
+            complete: false,
+            approvalCount: 0
         });
 
         requests.push(newRequest);
     }
 
-    function approveRequest(uint index) public {
+    function approveRequest(uint256 index) public {
         Request storage request = requests[index];
 
         require(approvers[msg.sender]);
@@ -82,7 +86,7 @@ contract Campaign {
         request.approvalCount++;
     }
 
-    function finalizeRequest(uint index) public restricted {
+    function finalizeRequest(uint256 index) public restricted {
         Request storage request = requests[index];
 
         require(request.approvalCount > (approversCount / 2));
@@ -91,4 +95,30 @@ contract Campaign {
         request.recipient.transfer(request.value);
         request.complete = true;
     }
+
+    function getCampaignDetails()
+        public
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            address
+        )
+    {
+        // retrieve the balance, minimum contribution,
+        return (
+            minimumContribution,
+            this.balance,
+            requests.length,
+            approversCount,
+            manager
+        );
+    }
+
+    // function getAllRequests() public view returns (uint256[]) {
+
+    //     return [ 33 ];
+    // }
 }
--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,41 +1,45 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const Web3 = require('web3');
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
 // const Web3EthContract = require('web3-eth-contract');
-const compiledFactory = require('./build/CampaignFactory.json');
+const compiledFactory = require("./build/CampaignFactory.json");
 
 // edit .env file.
-require('dotenv').config();
+require("dotenv").config();
 const provider = new HDWalletProvider(
   process.env.METAMASK_MNEMONIC,
-  process.env.INFURA_ENDPOINT,
+  process.env.INFURA_ENDPOINT
 );
 // Web3EthContract.setProvider(provider);
 const web3 = new Web3(provider);
 
 const deploy = async () => {
-    const accounts = await web3.eth.getAccounts();
+  const accounts = await web3.eth.getAccounts();
 
-    console.log('Attempting to deploy from account', accounts[0]);
+  console.log("Attempting to deploy from account", accounts[0]);
 
-    const contract = new web3.eth.Contract(JSON.parse(compiledFactory.interface), null, {
-        data: compiledFactory.bytecode,
-        from: accounts[0],
-        gas: '1000000', 
-    });
+  const contract = new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface),
+    null,
+    {
+      data: compiledFactory.bytecode,
+      from: accounts[0],
+      gas: "1000000",
+    }
+  );
 
-//    var contract = new web3.eth.Contract( JSON.parse(compiledFactory.interface) );
-    console.log('contract: ', contract);
-    const contractReady = await contract.deploy({
-                        data: compiledFactory.bytecode
-                    });
-    const estimatedGas = await contractReady.estimateGas();
+  //    var contract = new web3.eth.Contract( JSON.parse(compiledFactory.interface) );
+  //   console.log('contract: ', contract); Very long data to display on terminal.
+  const contractReady = await contract.deploy({
+    data: compiledFactory.bytecode,
+  });
+  const estimatedGas = await contractReady.estimateGas();
 
-    const result = await contractReady.send({
-        from: accounts[0],
-        gas: estimatedGas
-    });
+  const result = await contractReady.send({
+    from: accounts[0],
+    gas: estimatedGas,
+  });
 
-    console.log('Contract deployed to', result.options.address);
-    provider.engine.stop();
-}
+  console.log("Contract deployed to", result.options.address);
+  provider.engine.stop();
+};
 deploy();
